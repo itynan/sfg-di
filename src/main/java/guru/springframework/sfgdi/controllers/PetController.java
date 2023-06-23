ef426d142ea7e9c7e5ef6b9beb344a5a07ed1282
Iliya Tynan <itynan89@gmail.com>
2023-06-23 16:22:11 -0400

    resolved DI issue assignment

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,13 +1,18 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Service
 public class PetController {
-
     private final PetService petService;
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
 
     public String whichPetIsTheBest(){
         return petService.getPetType();
